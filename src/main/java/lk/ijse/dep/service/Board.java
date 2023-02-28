77614b300f4cb21f717adb7a5e713ce381bfdaef
IroshPerera <iroshlashan31@gmail.com>
2023-02-28 23:46:18 +0530

    updateMove method overload

--- src/main/java/lk/ijse/dep/service/Board.java
+++ src/main/java/lk/ijse/dep/service/Board.java
@@ -1,14 +1,15 @@
 package lk.ijse.dep.service;
 
 public interface Board{
-    public int NUM_OF_ROWS=5;
-    public int NUM_OF_COLS=6;
+     int NUM_OF_ROWS=5;
+     int NUM_OF_COLS=6;
 
      BoardUI getBoardUI();
      int findNextAvailableSpot(int col);
      boolean isLegalMove(int col);
      boolean existLegalMove();
      void updateMove(int col,Piece move);
+     void updateMove(int col,int row,Piece move);
      Winner findWinner();
 
 
