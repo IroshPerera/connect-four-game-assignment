d92437adfef2b208b9c940666373855fca749a11
IroshPerera <iroshlashan31@gmail.com>
2023-02-28 23:44:33 +0530

    add minimax algorithm

--- src/main/java/lk/ijse/dep/service/AiPlayer.java
+++ src/main/java/lk/ijse/dep/service/AiPlayer.java
@@ -1,32 +1,59 @@
 package lk.ijse.dep.service;
 
-import java.util.Random;
 
-public class AiPlayer extends Player{
-
-    public AiPlayer(Board newBoard) {
-    this.board=newBoard;
+public class AiPlayer extends Player {
+    public AiPlayer(Board board) {
+        this.board=board;
     }
+
     @Override
     public void movePiece(int col){
-        Random random =new Random();
-        int num;
-        boolean legalMove;
-        do {
 
-            num = random.nextInt(6);
-            legalMove= board.isLegalMove(num);
+        col=minMax();
+        board.updateMove(col, Piece.GREEN);
+        board.getBoardUI().update(col, false);
+        Winner winner = board.findWinner();
+        if(winner.getWinningPiece().equals(Piece.BLUE)||winner.getWinningPiece().equals(Piece.GREEN)) {
+            board.getBoardUI().notifyWinner(winner);
+        }
 
-        }while (!legalMove);
+    }
 
-        if (legalMove){
-            board.updateMove(num, Piece.GREEN);
-            board.getBoardUI().update(num, false);
-            Winner winner = board.findWinner();
-            if(winner.getWinningPiece().equals(Piece.BLUE)||winner.getWinningPiece().equals(Piece.GREEN)) {
-                board.getBoardUI().notifyWinner(winner);
+
+    private int minMax() {
+        for (int i = 0; i < 6; i++) {
+            if(board.isLegalMove(i)){
+                int row = board.findNextAvailableSpot(i);
+                board.updateMove(i,Piece.GREEN);
+                if(board.findWinner().getWinningPiece().equals(Piece.GREEN)){
+                    board.updateMove(i,row,Piece.EMPTY);
+                    return i;
+                }else{
+                    board.updateMove(i,row,Piece.EMPTY);
+                }
+            }
+
+        }for (int i = 0; i < 6; i++) {
+            if(board.isLegalMove(i)){
+                int row = board.findNextAvailableSpot(i);
+                board.updateMove(i,Piece.BLUE);
+                if(board.findWinner().getWinningPiece().equals(Piece.BLUE)){
+                    board.updateMove(i,row,Piece.EMPTY);
+                    return i;
+
+                }else{
+                    board.updateMove(i,row,Piece.EMPTY);
+                }
             }
 
         }
+        int num;
+        do{
+            num= (int) (Math.random() * 6);
+        }while (!board.isLegalMove(num));
+
+        return num;
     }
+
+
 }
