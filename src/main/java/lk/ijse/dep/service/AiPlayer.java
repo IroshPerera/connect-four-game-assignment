e63d7f04ea4c16bd0e6a203ee3925438ef24a200
IroshPerera <iroshlashan31@gmail.com>
2023-03-02 20:09:12 +0530

    add another minmax algorithm

--- src/main/java/lk/ijse/dep/service/AiPlayer.java
+++ src/main/java/lk/ijse/dep/service/AiPlayer.java
@@ -6,7 +6,7 @@ public class AiPlayer extends Player {
         this.board=board;
     }
 
-    @Override
+   @Override
     public void movePiece(int col){
 
         col=minMax();
@@ -54,6 +54,95 @@ public class AiPlayer extends Player {
 
         return num;
     }
+    /*public void movePiece(int col) {
+        col = this.predictColumn();
+        this.board.updateMove(col, Piece.GREEN);
+        this.board.getBoardUI().update(col, false);
+        Winner winner = this.board.findWinner();
+        if (winner.getWinningPiece() != Piece.EMPTY) {
+            this.board.getBoardUI().notifyWinner(winner);
+        } else if (!this.board.existLegalMove()) {
+            this.board.getBoardUI().notifyWinner(new Winner(Piece.EMPTY));
+        }
 
+    }
+
+    private int predictColumn() {
+        boolean isUserWinning = false;
+        int tiedColumn = 0;
+
+        int i;
+        for(i = 0; i < 6; ++i) {
+            if (this.board.isLegalMove(i)) {
+                int row = this.board.findNextAvailableSpot(i);
+                this.board.updateMove(i, Piece.GREEN);
+                int heuristicVal = this.minimax(0, false);
+                this.board.updateMove(i, row, Piece.EMPTY);
+                if (heuristicVal == 1) {
+                    return i;
+                }
+
+                if (heuristicVal == -1) {
+                    isUserWinning = true;
+                } else {
+                    tiedColumn = i;
+                }
+            }
+        }
+
+        if (isUserWinning && this.board.isLegalMove(tiedColumn)) {
+            return tiedColumn;
+        } else {
+
+
+            do {
+                i = (int)(Math.random() * 6.0D);
+            } while(!this.board.isLegalMove(i));
+
+            return i;
+        }
+    }
+
+    private int minimax(int depth, boolean maximizingPlayer) {
+        Winner winner = this.board.findWinner();
+        if (winner.getWinningPiece() == Piece.GREEN) {
+            return 1;
+        } else if (winner.getWinningPiece() == Piece.BLUE) {
+            return -1;
+        } else if (this.board.existLegalMove() && depth != 2) {
+            int i;
+            int row;
+            int heuristicVal;
+            if (!maximizingPlayer) {
+                for(i = 0; i < 6; ++i) {
+                    if (this.board.isLegalMove(i)) {
+                        row = this.board.findNextAvailableSpot(i);
+                        this.board.updateMove(i, Piece.BLUE);
+                        heuristicVal = this.minimax(depth + 1, true);
+                        this.board.updateMove(i, row, Piece.EMPTY);
+                        if (heuristicVal == -1) {
+                            return heuristicVal;
+                        }
+                    }
+                }
+            } else {
+                for(i = 0; i < 6; ++i) {
+                    if (this.board.isLegalMove(i)) {
+                        row = this.board.findNextAvailableSpot(i);
+                        this.board.updateMove(i, Piece.GREEN);
+                        heuristicVal = this.minimax(depth + 1, false);
+                        this.board.updateMove(i, row, Piece.EMPTY);
+                        if (heuristicVal == 1) {
+                            return heuristicVal;
+                        }
+                    }
+                }
+            }
+
+            return 0;
+        } else {
+            return 0;
+        }
+    }*/
 
 }
